1a8a2881cf8b1703669887b74cf7bf35c06d590f
davidL <david.lafreniere@live.ca>
2018-10-08 14:28:25 -0400

    little reformating

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :games
+  
   root to: "games#index"
 end
