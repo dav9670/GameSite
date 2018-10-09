16561beadc94779336fed711a97bd879affbd495
davidL <david.lafreniere@live.ca>
2018-10-09 14:34:37 -0400

    Before participants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Game.create!(:name => 'Fortnite.exe')
+Game.create!(:name => 'Habbo')
+Game.create!(:name => 'Club penguin')
+Game.create!(:name => 'OSRS')
+Game.create!(:name => 'Wizard 101')
+Game.create!(:name => '8 ball pool')
 
 User.create!(:firstname => 'John', :lastname => 'Doe', :email => 'john@gmail.com', :password => 'topsecret')
 
