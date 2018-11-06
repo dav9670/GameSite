75bc889b23c6c09921964eda83870813b521e048
david <david.lafreniere@live.ca>
2018-11-06 10:07:21 -0500

    show square

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,3 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery3
-//= require jquery_ujs
-//= require_tree .
--- app/assets/javascripts/participants.js
+++ app/assets/javascripts/participants.js
@@ -3,22 +3,23 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 class Square {
-	constructor(value){
+	constructor(value, color){
 		this.value = value;
+		this.color = color;
 	}
 }
 
 class Board {
-	constructor (width, height) {
-		this.width = width;
-		this.height = height;
-		this.length = 4;
+	constructor (length, offestX, offsetY) {
+		this.length = length
+		this.offestX = offestX;
+		this.offsetY = offsetY;
+		this.nbSquares = 4;
 		this.grid = [];
-		for(let i=0; i<this.length; i++){
+		for(let i=0; i<this.nbSquares; i++){
 			let column = [];
-			for(let x=0; x<this.length; x++){
-				console.log(x);
-				column[x] = new Square(0);
+			for(let x=0; x<this.nbSquares; x++){
+				column[x] = new Square(0, "green");
 			}
 			this.grid[i] = column;
 		}
@@ -27,17 +28,21 @@ class Board {
 	draw(context) {
 		for(let i=0; i<this.grid.length; i++){
 			for(let x=0; x<this.grid[i].length; x++){
-				let sWidth = this.width / this.length;
-				let sHeight = this.height / this.length;
-				let startX = x * sWidth;
-				let startY = i * sHeight;
-				console.log(sWidth + " " + sHeight);
-				context.moveTo(startX, startY);
-				context.lineTo(startX + sWidth, startY);
-				context.lineTo(startX + sWidth, startY + sHeight);
-				context.lineTo(startX, startY + sHeight);
-				context.lineTo(startX, startY);
+				let sLength = this.length / this.nbSquares;
+				let startX = x * sLength + this.offestX;
+				let startY = i * sLength + this.offsetY;
+
+				context.beginPath();
+				context.rect(startX, startY, sLength, sLength);
+				context.fillStyle = this.grid[i][x].color;
+				context.fill();
 				context.stroke();
+
+				context.textAlign = "center";
+				context.textBaseline = "middle";
+				context.font = "20px Arial";
+				context.fillStyle = "black";
+				context.fillText(this.grid[i][x].value, startX + (sLength / 2), startY + (sLength / 2), sLength);
 			}
 		}
 	}
@@ -48,10 +53,11 @@ let canvas;
 $(document).ready(function () {
 	canvas = document.getElementById('game_canvas');
 
-	let board = new Board(canvas.offsetWidth, canvas.offsetHeight);
-	board.draw(canvas.getContext('2d'));
-
-	console.log(board);
+	let currentBoard = new Board(canvas.offsetWidth / 3, canvas.offsetWidth / 15, canvas.offsetHeight / 15);
+	let opponentBoard = new Board(canvas.offsetWidth / 3, canvas.offsetWidth / 15 * 14 - (canvas.offsetWidth / 3), canvas.offsetHeight / 15);
+	
+	currentBoard.draw(canvas.getContext('2d'));
+	opponentBoard.draw(canvas.getContext('2d'));
 
 	//canvas.addEventListener('mousedown', doMouseDown)
 });
